4cb8a23a9d66310190789a752d7fdab0653c3a90
21s123-eng <hxlcvbn123@gmail.com>
2026-04-10 11:45:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log("Bot is online!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "السلام") {
+    message.reply("وعليكم السلام 👋");
+  }
+});
+
+client.login(process.env.TOKEN);
